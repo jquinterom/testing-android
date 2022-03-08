6376df7902677b33837a312c85261a2e2222c0f3
jquinterom <jfredyquintero@hotmail.com>
2022-03-08 08:19:36 -0500

    Prueba de farmentos

---
+++ Inventory/app/src/test/java/com/cursosandroidant/inventory/addModule/view/AddProductFragmentTest.kt
@@ -0,0 +1,37 @@
+package com.cursosandroidant.inventory.addModule.view
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import androidx.fragment.app.testing.launchFragment
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.cursosandroidant.inventory.R
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AddProductFragmentTest {
+    @Test
+    fun createDialog_notNullTest() {
+        val scenario =
+            launchFragment<AddProductFragment>(themeResId = R.style.Theme_Inventory) // importante definir el tema
+        scenario.moveToState(Lifecycle.State.RESUMED) // Fargment ya visible
+        scenario.onFragment { fragment ->
+            assertThat(fragment.dialog, `is`(notNullValue()))
+        }
+    }
+
+    @Test
+    fun cancelDialog_isNullTest() {
+        val scenario = launchFragment<AddProductFragment>(themeResId = R.style.Theme_Inventory)
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onFragment { fragment ->
+            (fragment.dialog as? AlertDialog)?.let {
+                it.getButton(DialogInterface.BUTTON_NEGATIVE).performClick()
+                assertThat(fragment.dialog, `is`(nullValue()))
+            }
+        }
+    }
+}
