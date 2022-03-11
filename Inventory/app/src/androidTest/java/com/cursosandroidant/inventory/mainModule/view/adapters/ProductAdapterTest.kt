326aff526c47d48fb55d563547571fbc0389b210
jquinterom <jfredyquintero@hotmail.com>
2022-03-11 10:14:33 -0500

    Test sobre elemento seleccionado de recyclerview

--- Inventory/app/src/androidTest/java/com/cursosandroidant/inventory/mainModule/view/adapters/ProductAdapterTest.kt
+++ Inventory/app/src/androidTest/java/com/cursosandroidant/inventory/mainModule/view/adapters/ProductAdapterTest.kt
@@ -2,8 +2,10 @@ package com.cursosandroidant.inventory.mainModule.view.adapters
 
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -24,7 +26,17 @@ class ProductAdapterTest {
 
     @Test
     fun listItem_click_successCheck() {
+        // Click sobre el segundo elemento
         Espresso.onView(withId(R.id.recyclerView))
             .perform(actionOnItemAtPosition<ProductAdapter.ViewHolder>(1, click()))
+
+        // Validar que el texto que se seleccionó sea "Queso"
+        Espresso.onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText("Queso")))
+    }
+
+    @Test
+    fun listItem_longClick_removeFromView(){
+
     }
 }
